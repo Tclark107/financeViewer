4a3a2e30deb95151d1b3844b977f6ae2be743a39
Tristan Clark <tcjc107@gmail.com>
2022-04-16 22:37:30 -0400

    I started the testManager and made a good base for the project

---
+++ FinanceManagerTester.py
@@ -0,0 +1,11 @@
+import unittest
+
+class testFinanceManager(unitTest.TestCase):
+    def(test_net)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- financeViewer.py
+++ financeViewer.py
@@ -2,9 +2,13 @@ import csv
 import math
 
 global net
+global expenseTotal
+global outputFile
 
 def csvToList():
-    with open('./2021novtoDec.csv', newline='') as f:
+    #with open('./2021novtoDec.csv', newline='') as f:
+    #with open('./202201to04.csv', newline='') as f:
+    with open('./41022.csv', newline='') as f:
         reader = csv.reader(f)
         transactions = list(reader)
     return transactions
@@ -22,45 +26,15 @@ def parseDescription(transactions):
         description.append(descriptionSplit)
     return transactions
 
-#def parseByWord(transactions):
-#   unknown = list()
-#   for transaction in transactions:
-#      for word in transaction[4]:
-#         if(word == 'SAVE'):
-#              savings += float(transaction[1])
-#         elif(word == 'TROY'):
-#              nonNecessaryFood += float(transaction[1])
-#         elif(word == 'UBER'):
-#              uber += float(transaction[1])
-#         elif(word == 'ONLYFANS.COM'):
-#              hdog += float(transaction[1])
-#         elif(word == 'MDW'):
-#              nonNecessaryFood += float(transaction[1])
-#         elif(word == 'CIAO'):
-#              nonNecessaryFood += float(transaction[1])
-#         elif(word == 'CARLS'):
-#              nonNecessaryFood += float(transaction[1])
-#         elif(word == 'LOCKHEED'):
-#              salary += float(transaction[1])
-#         elif(word == 'CVS/PHARM'):
-#              medical += float(transaction[1])
-#         elif(word == 'CHICAGONEWSST1725'):
-#              nonNecessaryFood += float(transaction[1])
-#         # maybe travel should be seperated out more
-#         # maybe fore some kinds of transactions where
-#         # we have multiple kinds of transactions we should
-#         # make another category
-#         # like food
-
 #I am going to overload this
-def checkThatWeKeepTrackOfAllTransactions():
-    incomeTotal = calculateNet(income)
-    expenseTotal = calculateNet(expense)
-    total = incomeTotal + expenseTotal
-    delta = net - total
-    delta = math.sqrt(delta * delta)
-    if(delta >= 0.001):
-        print("total != net")
+#def checkThatWeKeepTrackOfAllTransactions():
+#   incomeTotal = calculateNet(income)
+#   expenseTotal = calculateNet(expense)
+#   total = incomeTotal + expenseTotal
+#   delta = net - total
+#   delta = math.sqrt(delta * delta)
+#   if(delta >= 0.001):
+#       print("total != net")
 
 def seperateIntoIncomeAndExpenses(transactions):
     global net
@@ -74,90 +48,108 @@ def seperateIntoIncomeAndExpenses(transactions):
     return income, expense
     
 def seperateExpensesIntoCategories(expenses):
-    necessarySpending = list()
-    entertainment     = list()
-    savings           = list()
-    unknown           = list() 
-    # I may want another called food because seperating
-    # it between necessity and entertainment is tough
+    necessarySet = {"CVS/PHARM","646-846-3663","PECO","TARGET","COMCAST","MTA*METROCARD","EZPASS","Rachael"}
+    entertainmentSet     = {"UBER","CARLS","WAWA","WINE","SPIRITS","MARTHA","FIRSTLEAF","WHELIHAN","TROY","YOSH","WEGMANS","PlaystationNetwork","CheU","TRESTLE","BAR-LY","BREW","PANERA","DUNKIN","DOORDASH","SOUTHWES","TGI","AKI","PARKMOBILE","BAKESHOP","CAVANAUGHS","OLDCITYCOFFEE"}
+    savingsSet           = {"SAVE","MONEYLINE"}
 
-    # I think that I want to rework this where
-    # I will have a couple different dictionaries and
-    # if word is in any of the dictionaries then put it into a list
-    # that way rather than updating this, I can update the dictionary
-    # plus looking up in a dictionary is going to be faster
+    necessaryList = list()
+    entertainmentList     = list()
+    savingsList           = list()
+    unknownList           = list()
+
+    # I need to find a way to make this more robustso that I don't
+    # end up in a situation where I append something to a list where
+    # it shouldn't be
+    # maybe write tests around this
+    count =0
+    processMemo = list()
+    expenseHandledFlag = 0
     for expense in expenses:
-        for word in expense[4]:
-            if(word == 'SAVE'):
-                savings.append(expense)
-                del expense # issue here is if I start deleting something
-                            # with save in it that is also something else
-                            #maybe I put another if check here? 
-#           elif word == 'TROY':
-#               entertainment.append(expense)
-#               del expense
-#           elif(word == 'UBER'):
-#               entertainment.append(expense)
-#               del expense
-#           elif(word == 'SPIRITS'):
-#               unknown.append(expense)
-#               del expense
-#           elif(word == 'CIAO'):
-#               entertainment.append(expense)
-#               del expense
-#           elif(word == 'CARLS'):
-#               entertainment.append(expense)
-#               del expense
-#           elif(word == 'WAWA'):
-#               entertainment.append(expense)
-#               del expense
-#           elif(word == 'CVS/PHARM'):
-#               necessarySpending.append(expense)
-#               del expense
-#           elif(word == 'CHICAGONEWSST1725'):
-#               entertainment.append(expense)
-#               del expense
-#           elif(word == 'HLLFRSH'):
-#               necessarySpending.append(expense)
-#               del expense
-#           elif(word == 'ONLYFANS'):
-#               entertainment.append(expense)
-#               del expense
-#           elif(word == 'MARTHA'):
-#               entertainment.append(expense)
-#               del expense
-#           elif(word == 'TALW'):
-#               unknown.append(expense)
-#               del expense
-#           elif(word == 'PlaystationNetwork'):
-#               entertainment.append(expense)
-#               del expense
-#           elif(word == 'WEGMANS'):
-#               entertainment.append(expense)
-#               del expense
-            else:
-                unknown.append(expense)
-                del expense
-            break
+        count += 1
+        for word in expense[4]: 
+            if word in necessarySet:
+                necessaryList.append(expense)
+                expenseHandledFlag = 1
+                break
+            elif word in entertainmentSet:
+                entertainmentList.append(expense)
+                expenseHandledFlag = 1
+                break
+            elif word in savingsSet:
+                savingsList.append(expense)
+                expenseHandledFlag = 1
+                break
+        if(not expenseHandledFlag):
+            unknownList.append(expense)
+        expenseHandledFlag = 0
 
-    ukTotal = calculateNet(unknown)
-    entTotal = calculateNet(entertainment)
-    nSTotal = calculateNet(necessarySpending)
-    sTotal = calculateNet(savings)
-    total = nSTotal + sTotal + entTotal + ukTotal
-    return total
+    print(count)
+    print (len(expenses))
+    return necessaryList, entertainmentList, savingsList, unknownList
+    #if count does not = amount of expenses throw an error
+
+def calculatePercentage(money):
+    global expenseTotal
+    percent = money/expenseTotal
+    percent = int(percent*100)
+    return percent
 
 def calculateFinancials(transactions):
+    global expenseTotal
     income, expense = seperateIntoIncomeAndExpenses(transactions)
     incomeTotal = calculateNet(income)
+    expenseTotal = calculateNet(expense)
+    print(expenseTotal)
     print(incomeTotal)
+    Total = incomeTotal + expenseTotal
+    print(Total)
     
-    total = seperateExpensesIntoCategories(expense)
-    print(incomeTotal + total)
+    necessaryList, entertainmentList, savingsList, unknownList = seperateExpensesIntoCategories(expense)
 
-#def initializeTotals():
-#    savings, nonNecessaryFood, necessaryFood, alcohol,
-#    bars, uber, hdog, salary, medical = 0
+    necessaryTotal = calculateNet(necessaryList)
+    entertainmentTotal= calculateNet(entertainmentList)
+    savingsTotal = calculateNet(savingsList)
+    unknownTotal = calculateNet(unknownList)
+
+    necessaryPercent     = calculatePercentage(necessaryTotal)
+    entertainmentPercent = calculatePercentage(entertainmentTotal)
+    savingsPercent       = calculatePercentage(savingsTotal)
+    unknownPercent       = calculatePercentage(unknownTotal)
+
+    # print everything to screenout
+    print("You spent " , necessaryTotal, " on necessary things")
+    print("You spent " , entertainmentTotal ," on entertainment things")
+    print("You spent " , savingsTotal , " on savingsthings")
+    print("You spent " , unknownTotal , " on unknown things")
+    print("Here is your unknown list of things")
+    for element in unknownList:
+        print(element)
+    print("necpercentage is ", necessaryPercent, "%\n")
+    print("entpercentage is ", entertainmentPercent, "%\n")
+    print("savpercentage is ", savingsPercent, "%\n")
+    print("unkpercentage is ", unknownPercent, "%\n")
+    print("You made" , incomeTotal) 
+    print("You spent" , expenseTotal) 
+    print("Your net is" , Total) 
+
+    # write everyting to a file
+    outputFile = open("041022.txt","w")
+    outputFile.write("You spent " + str(calculateNet(necessaryList))+ " on necessary things\n")
+    outputFile.write("You spent " + str(calculateNet(entertainmentList))+" on entertainment things\n")
+    outputFile.write("You spent " + str(calculateNet(savingsList)) + " on savingsthings\n")
+    outputFile.write("You spent " + str(calculateNet(unknownList)) + " on unknown things\n")
+    outputFile.write("Here is your unknown list of things\n")
+    for element in unknownList:
+        outputFile.write(str(element)+"\n")
+    outputFile.write("necpercentage is "+ str(necessaryPercent)+"\n")
+    outputFile.write("entpercentage is "+ str(entertainmentPercent)+"\n")
+    outputFile.write("savpercentage is "+ str(savingsPercent)+"\n")
+    outputFile.write("unkpercentage is "+ str(unknownPercent)+"\n")
+    outputFile.write("You made" + str(incomeTotal)+"\n")
+    outputFile.write("You spent" + str(expenseTotal)+"\n") 
+    outputFile.write("Your net is" + str(Total))
+
+    outputFile.close()
 
 def main():
     global net
